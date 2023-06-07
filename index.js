5a92669589a97b0a7661a046742371dc93c9c61b
navyn13 <121725696+navyn13@users.noreply.github.com>
2023-06-07 17:26:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,5 +14,54 @@ for( var i=0; i<possibleChoices.length; i++){
     possibleChoices[i].addEventListener('click', function(e){
         userChoice=e.target.innerHTML;
         userChoiceDisplay.innerHTML = userChoice
+        generateComputerChoice()
     })
+}
+function generateComputerChoice(){
+    var randomNumber= Math.floor(Math.random()*3 ) + 1;
+    if(randomNumber===1){
+        computerChoice =  'Stone🗿'
+    }
+    if(randomNumber===2){
+        computerChoice =  'Paper📔'
+    }
+    if(randomNumber===3){
+        computerChoice =  'Scissor✂️'
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
+    if(userChoice=== 'Stone🗿'){
+        if(computerChoice==='Paper📔'){
+            resultDisplay.innerHTML = 'You Lost😑'
+        }
+        else if(computerChoice==='Scissor✂️'){
+            resultDisplay.innerHTML = 'You Won🎉'
+        }
+        else{
+            resultDisplay.innerHTML = 'DRAW'
+        }
+
+    }
+
+if(userChoice=== 'Paper📔'){
+    if(computerChoice==='Scissor✂️'){
+        resultDisplay.innerHTML = 'You Lost😑'
+    }
+    else if(computerChoice==='Stone🗿'){
+        resultDisplay.innerHTML = 'You Won🎉'
+    }
+    else{
+        resultDisplay.innerHTML = 'DRAW'
+    }
+}
+if(userChoice=== 'Scissor✂️'){
+    if(computerChoice==='Stone🗿'){
+        resultDisplay.innerHTML = 'You Lost😑'
+    }
+    else if(computerChoice==='Paper📔'){
+        resultDisplay.innerHTML = 'You Won🎉'
+    }
+    else{
+        resultDisplay.innerHTML = 'DRAW'
+    }
+}
 }
